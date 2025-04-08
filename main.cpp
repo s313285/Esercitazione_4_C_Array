07363082d943d6674a99592625b9f860707e0d83
s313285 <s313285@studenti.polito.it>
2025-04-08 11:55:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {
-	std::string inputFile = "data.txt";
+    std::string inputFile = "data.txt";
     size_t n;
-	double S;
+    double S;
     double *p_w = nullptr;
     double *p_r = nullptr;
 	
@@ -16,14 +16,14 @@ int main()
         return 1; // esecuzione del programma non terminata con successo
 	
 	
-	double V = final_value(n,S,p_w,p_r);
-	double r_tot = rate_of_return(S,V);
+     double V = final_value(n,S,p_w,p_r);
+     double r_tot = rate_of_return(S,V);
 	
 	
-	std::string outputFile = "result.txt";
+     std::string outputFile = "result.txt";
 	
-	if(!ExportResult(outputFile, n, S, p_w, p_r, V, r_tot))
-        return 2; // esecuzione del programma non terminata con successo
+     if(!ExportResult(outputFile, n, S, p_w, p_r, V, r_tot))
+         return 2; // esecuzione del programma non terminata con successo
 	
     return 0;
 }
