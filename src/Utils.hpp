271959b16efd0cd4e23c28de66f82e42ea415af4
s313285 <s313285@studenti.polito.it>
2025-04-08 11:52:42 +0200

    Update Utils.hpp

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -10,7 +10,7 @@ using namespace std;
 // return the result of the reading, true is success, false is error
 bool ImportVectors(const string& inputFilePath,
                    size_t& n,
-				   double& S,
+		   double& S,
                    double*& w,
                    double*& r);
 
@@ -28,7 +28,7 @@ double final_value(size_t& n,
 // S: total invested sum	
 // V: result of the final_value(...)
 double rate_of_return (double& S,
-					   double& V);
+		       double& V);
 
 
 // ExportResult export the result obtained in file
@@ -37,11 +37,11 @@ double rate_of_return (double& S,
 // return the result of the export, true is success, false is error
 bool ExportResult(const string& outputFilePath,
                   size_t& n,
-				  double& S,
-				  double*& w,
+		  double& S,
+		  double*& w,
                   double*& r,
-				  double& V,
-				  double& r_tot);
+		  double& V,
+		  double& r_tot);
 
 
 // Export a vector in a string
